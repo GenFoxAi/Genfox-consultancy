d10986587027c053b69526eb2227eb1bbc7b7ed2
zoro-diablo <vyshnavk117@gmail.com>
2025-03-03 11:36:57 +0530

    update Toad component to adjust animation end value for improved responsiveness on larger screens

--- src/components/Toad.jsx
+++ src/components/Toad.jsx
@@ -26,7 +26,7 @@ export default function Toad() {
 
     const getEndValue = () => {
       const vh = window.innerHeight;
-      return window.innerWidth <= 768 ? `+=${vh * 2}` : `+=${vh * 1}`;
+      return window.innerWidth <= 768 ? `+=${vh * 2}` : `+=${vh * 0.5}`;
     };
 
     gsap.to(refs.current, {
